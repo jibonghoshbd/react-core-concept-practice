baf99c53aae98fe77141647700ed7b0dc64bc19a
jibonghoshbd <jibonghosh1510@gmail.comm>
2022-04-16 01:22:26 +0600

    react concept practice

--- src/App.js
+++ src/App.js
@@ -4,22 +4,23 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Blog name='Solim Uddim'></Blog>
+      <Blog name='Kolim Uddin'></Blog>
+      <Blog name="Abdur Alim Khan"></Blog>
+      <Blog name="Solyman Khan"></Blog>
     </div>
   );
+}
+
+function Blog(props) {
+  return (
+    <article className='blogs'>
+      <div className='blog'>
+        <h1 style={{ fontSize: 'bold', textTransform: 'uppercase' }}>Athor Name: {props.name} </h1>
+        <p style={{ color: 'blue', fontSize: '20px' }}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat consequuntur beatae distinctio sunt cumque quis dolor placeat esse doloremque laborum similique molestiae necessitatibus itaque animi ipsa eveniet dolore in exercitationem magni, nemo provident praesentium eos saepe? Numquam, cum quam quis consequuntur ad deleniti impedit delectus quos ducimus magni maiores et?</p>
+      </div>
+    </article>
+  )
 }
 
 export default App;
